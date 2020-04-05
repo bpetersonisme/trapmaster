a59b17e434cd8b6350feb6278494bbc5fcce0441
Josh-JD <60679969+Josh-JD@users.noreply.github.com>
2020-04-04 20:13:54 -0400

    Create Owls.java

---
+++ src/Owls.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Owls extends Monster_tm{
+	
+	public Owls() {
+		Random rmd = new Random();
+		health = 200;
+		loot = rmd.nextInt(120) + 30; 
+		attack = 3;
+	}
+	
+	public void attack() {};
+	public void move(){};
+	
+}
