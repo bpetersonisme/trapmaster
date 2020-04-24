4494dae168909d5dc2eb67a8001e6961f592d045
Josh-JD <60679969+Josh-JD@users.noreply.github.com>
2020-04-24 03:16:32 -0400

    Update Monster_tm.java

--- src/Monster_tm.java
+++ src/Monster_tm.java
@@ -1,24 +1,53 @@
-
-public class Monster_tm {
+public class Monster_tm extends RenderObj{
 	
 	protected int health;
 	protected int loot;
 	protected int attack;
 	
+	/*This takes a int that signifies how much damage monster will take.
+	The player object is just a basic class I made to test the function.
+	It passes the player option so once enemy dies, it goes to the death
+	function and adds the monster loot to player money pool.*/
+	
 	public void takeDmg(int dmgTaken, Player p){
 		this.health -= dmgTaken;
 		if(this.health <= 0) {
-			death(p);
+			//death(p);
 		}
 	}
 	
-	public void plunder(TreasureTile chest) {
-		chest.loseTreasure(attack);
-		this.loot += attack;
+	/*Takes a treasure tile object and calls its method to take loot.
+	 * Loot taken is based on the damage dealt to the treasure.
+	 */
+	public void plunder(TreasureTile ev) {
+		if (ev.getTreasure() > 0) {
+			if (this.attack >= ev.getTreasure()) {
+				this.loot += ev.getTreasure();
+				ev.loseTreasure(this.attack);
+				return;
+			}
+			ev.loseTreasure(this.attack);
+			this.loot += attack;
+		} else {
+			System.out.println("Looted everything, ogre now has: " + this.getLoot());
+		}
 	}
 	
+	/*Called when monster dies. Add monster loot to player money pool.
+	 * Not sure how to delete the sprite off the screen when it dies. 
+	 *
 	public void death(Player p) {
 		p.money += this.loot;
+	}*/
+	
+	
+	int count = 0;
+	public void death() {
+		cycleAnimation(4, 0, 1);
+		if (count == 3) {
+			this.setXPosWorld(-12200);
+		}
+		count++;
 	}
 	
 	public void attack() {}
@@ -27,6 +56,7 @@ public class Monster_tm {
 	
 	public void direction() {}
 	
+	/*Checks if monster is alive. */
 	public boolean isAlive() {
 		if(this.health > 0) {
 			return true;
